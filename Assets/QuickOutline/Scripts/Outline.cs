b837e3c0f154b70995995b463728e9b62209aedd
Lauris Taube <lauristaube@gmail.com>
2021-01-18 00:17:48 +0200

    Interactive tutorial base

--- Assets/QuickOutline/Scripts/Outline.cs
+++ Assets/QuickOutline/Scripts/Outline.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using UnityEngine;
 
 [DisallowMultipleComponent]
+
 public class Outline : MonoBehaviour {
   private static HashSet<Mesh> registeredMeshes = new HashSet<Mesh>();
 
---
+++ Assets/Scripts/InteractiveTutorial/DynamicGrabPoint.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+public class DynamicGrabPoint : MonoBehaviour
+{
+    private Vector3 m_InteractorPosition = Vector3.zero;
+    private Quaternion m_InteractorRotation = Quaternion.identity;
+
+    private XRGrabInteractable m_XRGrabInteractable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_XRGrabInteractable = GetComponent<XRGrabInteractable>();
+    }
+
+
+
+    public void OnSelectEnter(XRBaseInteractor interactor)
+    {
+        StoreInteractor(interactor);
+        MatchAttachmentPoints(interactor);
+    }
+
+    public void OnSelectExit(XRBaseInteractor interactor)
+    {
+        ResetAttachmentPoints(interactor);
+        ClearInteractor(interactor);
+    }
+
+
+
+    private void StoreInteractor(XRBaseInteractor interactor)
+    {
+        // Store Controller attachment point transform
+        m_InteractorPosition = interactor.attachTransform.localPosition;
+        m_InteractorRotation = interactor.attachTransform.localRotation;
+    }
+
+    // Sets the controllers attach point
+    private void MatchAttachmentPoints(XRBaseInteractor interactor)
+    {
+        // Check if this Interactable has an attach point
+        // If it does, then use that
+        // But if it doesn't, then use this Interactables' transform
+        bool hasAttach = m_XRGrabInteractable.attachTransform != null;
+        interactor.attachTransform.position = hasAttach ? m_XRGrabInteractable.attachTransform.position : transform.position;
+        interactor.attachTransform.rotation = hasAttach ? m_XRGrabInteractable.attachTransform.rotation : transform.rotation;
+    }
+
+    private void ResetAttachmentPoints(XRBaseInteractor interactor)
+    {
+        interactor.attachTransform.localPosition = m_InteractorPosition;
+        interactor.attachTransform.localRotation = m_InteractorRotation;
+    }
+
+    private void ClearInteractor(XRBaseInteractor interactor)
+    {
+        m_InteractorPosition = Vector3.zero;
+        m_InteractorRotation = Quaternion.identity;
+    }
+}
